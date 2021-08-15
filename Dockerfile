9fb0c572585657ab921facf18eea9abbbc3e6af1
arihant-2310 <arihant2310@gmail.com>
2021-08-15 15:45:44 +0530

    Fix docker compose file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:10
+FROM node:12.18.1
 WORKDIR /app
 COPY package.json /app
 RUN npm install
--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const ShortUrl = require("./src/models/UrlModel");
 app.use(cors());
 
 // view engine setup
-app.set("views", path.join(__dirname, "public"));
+app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");app.use(express.urlencoded({ extended: false }));
 
 const connection = require('./src/config/db.config');

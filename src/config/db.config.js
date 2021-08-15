41c3b9437b585805eba08fbc0e4834c79ab91081
arihant-2310 <arihant2310@gmail.com>
2021-08-15 15:01:59 +0530

    Fix env variable

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,16 +1,10 @@
-// import mongoose package
 const mongoose = require('mongoose')
+const DB_URI = process.env.MONGOURI;
 
-// declare a Database string URI
-const DB_URI = 'mongodb://localhost:27017/urlshortener'
-
-// establishing a database connection
 mongoose.connect(DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
 const connection = mongoose.connection
-
-// export the connection object
 module.exports = connection
